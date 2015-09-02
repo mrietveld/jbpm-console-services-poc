6975d18133bd29f8830f80bece3a02f81d5e0641
Marco Rietveld <mrietvel@redhat.com>
2015-09-02 13:47:25 +0200

    Last changes

--- process-event-listeners/src/main/java/org/custom/process/listeners/CustomProcessEventListenerProducer.java
+++ process-event-listeners/src/main/java/org/custom/process/listeners/CustomProcessEventListenerProducer.java
@@ -5,8 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import javax.inject.Inject;
-
 import org.jbpm.runtime.manager.api.qualifiers.Process;
 import org.kie.api.event.process.ProcessEventListener;
 import org.kie.api.runtime.manager.RuntimeManager;
@@ -16,21 +14,19 @@ import org.kie.internal.runtime.manager.EventListenerProducer;
  * See http://docs.jboss.org/jbpm/v6.2/userguide/jBPMCoreEngine.html#d0e2270
  */
 @Process
-public class CustomEventListenerProducer implements EventListenerProducer<ProcessEventListener> {
-
-    @Inject
-    private RuntimeManager runtimeManager;
+public class CustomProcessEventListenerProducer implements EventListenerProducer<CustomProcessEventListener> {
 
     @Override
-    public List<ProcessEventListener> getEventListeners( String identifier, Map<String, Object> params ) {
+    public List<CustomProcessEventListener> getEventListeners( String identifier, Map<String, Object> params ) {
         System.out.println( "Getting event listeners: " + identifier);
         for( String key : params.keySet() ) { 
             System.out.println( "Key: " + key);
         }
+        RuntimeManager runtimeManager = (RuntimeManager) params.get("runtimeManager");
         CustomProcessEventListener[] listenerArr = { new CustomProcessEventListener(runtimeManager) };
         // create a new ArrayList because Arrays.asList() returns an unmodifiable list
         // (and I'm not sure if the returned list will be modified somewhere?)
-        return new ArrayList<ProcessEventListener>(Arrays.asList(listenerArr));
+        return new ArrayList<CustomProcessEventListener>(Arrays.asList(listenerArr));
     }
 
 }
